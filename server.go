b64500bfd5b5f03bf232ff3ea8b6c72b029db9d4
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-08-14 08:34:08 -0600

    Added views and public folders

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 // type server struct {
@@ -12,10 +13,16 @@ import (
 // }
 
 func main() {
-	app := fiber.New()
+
+	// Initialize standard Go html template engine
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.Render("index", fiber.Map{})
 	})
 
 	log.Fatal(app.Listen(":3000"))
