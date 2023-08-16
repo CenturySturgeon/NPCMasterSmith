09f0c49b57a754abdea55299a6d013ccb5823f6b
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-08-16 12:16:27 -0600

    The go air ouput is now shown to the same terminal as the esbuild output

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,24 +1,25 @@
 import * as esbuild from 'esbuild'
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 
 // This file has a .mjs extension since its syntax would not work on a regular .js file due to node (refer to esbuild's documentation for more)
-
 let ctx = await esbuild.context({
     entryPoints: ["frontend/Application.tsx", "frontend/base.css"],
     outdir: "./public/esbundle/",
     bundle: true,
     minify: true,
     plugins: [],
-    })
+});
     
-await ctx.watch()
-console.log('⚡ Watching on Port: 8000 ⚡')
+await ctx.watch();
+console.log('⚡ Watching ⚡');
 
-// Since the package.json dev script can't run two commands at once, the server running command must be ran from here
-exec('cd server/ && air', (err, stdout, stderr) => {
-    if (err) {
-      console.error('Error running air command:', err);
-      return;
-    }
-    console.log(stdout);
-  });
+// Since the package.json dev script can't run two commands at once, the server running command must be ran from this script.
+const airProcess = spawn('air', [], { cwd: 'server/', stdio: 'inherit' });
+
+airProcess.on('error', (err) => {
+    console.error('Error running air command:', err);
+});
+
+airProcess.on('close', (code) => {
+    console.log(`air process exited with code ${code}`);
+});
