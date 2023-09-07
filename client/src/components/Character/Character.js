70926571483ec64a30493040796dcf76244b0a25
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-07 09:59:33 -0600

    Fixed styles of the epand button

--- client/src/components/Character/Character.js
+++ client/src/components/Character/Character.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
 import { Paper, Typography, Box, Collapse, IconButton } from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import React, { useState } from 'react';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import './Character.css'
 import man_image from '../../../public/images/profile_man.png'
@@ -17,29 +17,31 @@ export default function Character() {
     return (
         <Grid item xs={3}>
             <Paper elevation={3}>
-                <img className="img" src={man_image}></img>
+                <img className="img" src={man_image} alt="Character Image" />
                 <Box paddingX={1}>
+
                     <Typography variant="h6" component="h2">
                         Name of Character
                     </Typography>
                     <Typography variant="subtitle1">
                         "Hereby is thy quote, a brief phrase said by the character"
                     </Typography>
-                    
-                    <div style={{ display: 'flex' }}>
-                        <IconButton aria-label="add to favorites">
+
+                    {/* Flex container for icons */}
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        {/* FavoriteIcon on the left */}
+                        <IconButton>
                             <FavoriteIcon />
                         </IconButton>
-                        <div style={{ display: 'flex', justifyContent: 'right' }}>
-                            <IconButton
-                                onClick={handleExpandClick}
-                                aria-expanded={expanded}
-                                aria-label="show more"
-                                sx={{ transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)', justifySelf: 'end' }}
-                            >
-                                <ExpandMoreIcon />
-                            </IconButton>
-                        </div>
+
+                        <IconButton
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                            style={{ marginLeft: 'auto', transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)' }}
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
                     </div>
 
                     <Collapse in={expanded}>
