a9dc9d15aa68229f7b46312536cf8c9d17e80a7c
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-07 07:55:57 -0600

    Character component imports an image and its own css

--- client/src/components/Character/Character.js
+++ client/src/components/Character/Character.js
@@ -1,11 +1,15 @@
 import { Paper } from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
+import './Character.css'
+import image from '../../../public/images/profile_man.png'
 
 export default function Character() {
     return (
         <Grid item xs={3}>
-            <Paper elevation={3}>Character</Paper>
+            <Paper elevation={3}>
+                <img className="img" src={image}></img>
+            </Paper>
         </Grid>
     );
 }
