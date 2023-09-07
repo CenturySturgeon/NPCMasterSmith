57c38adcb364585163ef910f410d84193a867dde
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-06 21:10:23 -0600

    Added format

--- client/src/components/Character/CharacterLayout.js
+++ client/src/components/Character/CharacterLayout.js
@@ -4,12 +4,14 @@ import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Character from './Character';
 
 export default CharacterLayout = () => {
-    return (<Container>
-        <Grid container spacing={3}>
-            <Character></Character>
-            <Character></Character>
-            <Character></Character>
-            <Character></Character>
-        </Grid>
-    </Container>)
+    return (
+        <Container>
+            <Grid container spacing={3}>
+                <Character></Character>
+                <Character></Character>
+                <Character></Character>
+                <Character></Character>
+            </Grid>
+        </Container>
+    )
 }
