24d08973ba8c06e49672f8e19b388542fecf086e
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-10 17:23:11 -0600

    Collapse text subsection is now its own component

--- client/src/components/Character/CollapsableSection.js
+++ client/src/components/Character/CollapsableSection.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { Typography, Collapse, IconButton } from "@mui/material";
+
 import CollapseIcons from './CollapseIcons'
+import CollapseText from './CollapseText';
 
 
 const CollapsableSection = () => {
@@ -13,18 +14,7 @@ const CollapsableSection = () => {
     return (
         <div>
             <CollapseIcons handleExpandClick={handleExpandClick} isExpanded={isExpanded}></CollapseIcons>
-            <Collapse in={isExpanded}>
-                <Typography variant="body1">
-                    A brief, physical description of the character goes here.
-                </Typography>
-                <Typography variant="body1">
-                    <ul>
-                        <li>My first roleplay property.</li>
-                        <li>My second roleplay property.</li>
-                        <li>My third roleplay property.</li>
-                    </ul>
-                </Typography>
-            </Collapse>
+            <CollapseText isExpanded={isExpanded}></CollapseText>
         </div>
     )
 }
