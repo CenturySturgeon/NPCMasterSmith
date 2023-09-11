066b3b9ba4a019ce2130413637c980ed42c309a4
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-10 20:43:07 -0600

    Changed Characters layout to a dynamic display

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import CharactersLayout from "./components/Character/CharactersLayout";
 import PromptField from "./components/Prompt/PromptField";
 
+import man_image from '../public/images/profile_man.png'
+import woman_image from '../public/images/profile_woman.png'
+
 function Application() {
+
+    const dummyM = {image: man_image, name: "Name of Character", quote: "Hereby is thy quote, a brief phrase said by the character"};
+    const dummyF = {image: woman_image, name: "Name of Character", quote: "Hereby is thy quote, a brief phrase said by the character"};
+    const dummies = [dummyM, dummyF, dummyM, dummyF, dummyF];
+    
+
     return (
         <div>
             <div style={{display: 'flex', justifyContent: 'center', alignItems:'center', width: '100%' }}>
                 <PromptField></PromptField>
             </div>
             <br/>
-            <CharactersLayout></CharactersLayout>
+            <CharactersLayout items={dummies}></CharactersLayout>
         </div>
     )
 }
--- client/src/components/Character/CharactersLayout.js
+++ client/src/components/Character/CharactersLayout.js
@@ -3,17 +3,19 @@ import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 import Character from './Character';
 
-import man_image from '../../../public/images/profile_man.png'
-import woman_image from '../../../public/images/profile_woman.png'
-
-export default CharactersLayout = () => {
+export default CharactersLayout = (props) => {
     return (
         <Container>
             <Grid container spacing={3}>
-                <Character image={man_image} name={"Name of Character"} quote={"Hereby is thy quote, a brief phrase said by the character"}></Character>
-                <Character image={woman_image} name={"Name of Character"} quote={"Hereby is thy quote, a brief phrase said by the character"}></Character>
-                <Character image={man_image} name={"Name of Character"} quote={"Hereby is thy quote, a brief phrase said by the character"}></Character>
-                <Character image={woman_image} name={"Name of Character"} quote={"Hereby is thy quote, a brief phrase said by the character"}></Character>
+                {
+                    props.items.map((character) => (
+                        <Character
+                            image={character.image}
+                            name={character.name}
+                            quote={character.quote}
+                        />
+                    ))
+                }
             </Grid>
         </Container>
     )
