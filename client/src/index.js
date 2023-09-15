3f5c7d2c72bac93cdd2b97ce0f65b91821e94a55
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-15 12:42:24 -0600

    Started working with the custom theme

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,34 @@
 import ReactDOM from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import App from './App';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
 import './index.css'
 
-import App from './App';
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#19d2a2',
+        },
+        secondary: {
+            main: '#0099f5',
+        },
+        info: {
+            main: '#0288d1',
+        },
+        background: {
+            paper: '#282c34',
+        },
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(<App />);
+root.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+);
