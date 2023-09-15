97e56f2eaa1abdbdc72d718afc27c0c27b9595ff
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-15 13:21:34 -0600

    Enabled dark & light modes

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import App from './App';
 
 import '@fontsource/roboto/300.css';
@@ -8,7 +9,7 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.css'
 
-const theme = createTheme({
+const darkTheme = createTheme({
     palette: {
         mode: 'dark',
         primary: {
@@ -21,14 +22,43 @@ const theme = createTheme({
             main: '#0288d1',
         },
         background: {
+            default: '#282c34',
             paper: '#282c34',
         },
     },
 });
 
+const lightTheme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#19d2a2',
+        },
+        secondary: {
+            main: '#0099f5',
+        },
+        info: {
+            main: '#0288d1',
+        },
+        background: {
+            paper: '#fafafa',
+        },
+    },
+})
+
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
-    <ThemeProvider theme={theme}>
-        <App />
+    <ThemeProvider theme={darkTheme}>
+        <Box
+            sx={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                bgcolor: 'background.default',
+                color: 'text.primary',
+                padding: '0px'
+            }}
+        s>
+            <App />
+        </Box>
     </ThemeProvider>
 );
