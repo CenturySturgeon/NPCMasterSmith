d7598cddc4e019b5bdcd236c598bedb9037b9364
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-16 18:05:35 -0600

    Renamed the appbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import SearchAppBar from './components/AppBar/AppBar';
+import SearchAppBar from './components/SearchAppBar/AppBar';
 import Box from '@mui/material/Box';
 import { darkTheme, lightTheme } from './components/AppThemes/AppThemes'
 import ContenHolder from "./components/ContentHolder/ContentHolder";
