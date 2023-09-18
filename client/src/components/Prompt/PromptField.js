0d535b80d8007b492bdef9113aa17082c8ea39c4
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-18 07:02:54 -0600

    Added logic to send the prompt

--- client/src/components/Prompt/PromptField.js
+++ client/src/components/Prompt/PromptField.js
@@ -1,10 +1,23 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import SendIcon from '@mui/icons-material/Send';
 
 export default function PromptField() {
+
+  const [canSendPrompt, setCanSendPrompt] = useState(false);
+  const [prompt, setPrompt] = useState('');
+
+  function onPropmtInputChange(event){
+    setPrompt(event.target.value);
+    // Count the number of words in the prompt field
+    if (event.target.value.trim().split(/\s+/).length > 1){
+      setCanSendPrompt(true);
+      console.log('Prompt can be sent to the backend')
+    }
+  }
+
   return (
     <Box
       sx={{
@@ -18,6 +31,8 @@ export default function PromptField() {
         label="Create a character"
         id="fullWidth"
         helperText="A simple description of your character"
+        onChange={onPropmtInputChange}
+        defaultValue={prompt}
         InputProps={{
           endAdornment: (
             <InputAdornment position="end">
