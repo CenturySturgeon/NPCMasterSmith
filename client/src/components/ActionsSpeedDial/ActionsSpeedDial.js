c958465a7ed5b41cb67431a041212521c2e1795d
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-19 16:49:21 -0600

    Separation of concerns

---
+++ client/src/components/ActionsSpeedDial/ActionsSpeedDial.js
@@ -0,0 +1,38 @@
+import Box from '@mui/material/Box';
+// Icons
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+
+const actions = [
+    { icon: <FileCopyIcon />, name: 'Copy' },
+    { icon: <SaveIcon />, name: 'Save' },
+    { icon: <PrintIcon />, name: 'Print' },
+    { icon: <ShareIcon />, name: 'Share' },
+];
+
+const ActionsSpeedDial = () => {
+    return (
+        <Box sx={{ position: 'fixed', bottom: '16px', right: '24px' }}>
+            <SpeedDial
+                ariaLabel="Characters action speed dial"
+                icon={<SpeedDialIcon />}
+                sx={props.fixedItemMargin}
+
+            >
+                {actions.map((action) => (
+                    <SpeedDialAction
+                        key={action.name}
+                        icon={action.icon}
+                        tooltipTitle={action.name}
+                    />
+                ))}
+            </SpeedDial>
+        </Box>
+    )
+}
+export default ActionsSpeedDial
--- client/src/components/ContentHolder/ContentHolder.js
+++ client/src/components/ContentHolder/ContentHolder.js
@@ -1,49 +1,20 @@
+import ActionsSpeedDial from "../ActionsSpeedDial/ActionsSpeedDial";
 import CharactersLayout from "../Character/CharactersLayout";
 import PromptField from "../Prompt/PromptField";
 import Box from '@mui/material/Box';
 
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
-
-const actions = [
-    { icon: <FileCopyIcon />, name: 'Copy' },
-    { icon: <SaveIcon />, name: 'Save' },
-    { icon: <PrintIcon />, name: 'Print' },
-    { icon: <ShareIcon />, name: 'Share' },
-  ];
-
 const ContentHolder = (props) => {
     return (
         <Box
-            sx={{ mt: '12px'}}
+            sx={{ mt: '12px' }}
         >
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
                 <PromptField theme={props.theme}></PromptField>
             </div>
             <br />
             <CharactersLayout setBodyPadComp={props.setBodyPadComp} items={props.dummies}></CharactersLayout>
-            <Box sx={{position: 'fixed', bottom: '16px', right: '24px'}}>
-            <SpeedDial
-                ariaLabel="Characters action speed dial"
-                icon={<SpeedDialIcon />}
-                sx={props.fixedItemMargin}
-                
-            >
-                {actions.map((action) => (
-                    <SpeedDialAction
-                        key={action.name}
-                        icon={action.icon}
-                        tooltipTitle={action.name}
-                    />
-                ))}
-            </SpeedDial>
-            </Box>
-            
+            <ActionsSpeedDial fixedItemMargin={props.fixedItemMargin}></ActionsSpeedDial>
+
         </Box>
     )
 }
