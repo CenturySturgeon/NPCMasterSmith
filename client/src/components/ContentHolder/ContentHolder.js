b005f6755b18e1e1d614de781202630b6378806f
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-19 10:36:44 -0600

    Added floating 'add character' button

--- client/src/components/ContentHolder/ContentHolder.js
+++ client/src/components/ContentHolder/ContentHolder.js
@@ -1,6 +1,9 @@
 import CharactersLayout from "../Character/CharactersLayout";
 import PromptField from "../Prompt/PromptField";
 import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+
 
 const ContentHolder = (props) => {
     return (
@@ -12,6 +15,11 @@ const ContentHolder = (props) => {
             </div>
             <br />
             <CharactersLayout items={props.dummies}></CharactersLayout>
+            <Box sx={{position:'fixed', right:'16px', bottom:'12px'}}>
+                <Fab color="primary">
+                    <AddIcon></AddIcon>
+                </Fab>
+            </Box>
         </Box>
     )
 }
