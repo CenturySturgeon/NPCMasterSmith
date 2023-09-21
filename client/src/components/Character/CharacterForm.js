4d20034fc2526ff742c6eeaf26e65ed3ebc182a0
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-21 16:41:02 -0600

    Fixed the spacing between the card form inputs

--- client/src/components/Character/CharacterForm.js
+++ client/src/components/Character/CharacterForm.js
@@ -2,14 +2,15 @@ import { Box, TextField, Button } from "@mui/material";
 import './Character.css'
 
 export default function CharacterForm(props) {
+    const textFieldStyles = {marginBottom: '12px'};
     return (
         <form>
             <img className="img" src={props.image} alt="Character Image" />
             <Box padding={1}>
                 <div className="form-inputsHolder">
-                    <TextField label="Campaign" value={props.campaign} variant="outlined" />
-                    <TextField label="Name" value={props.name} variant="outlined" />
-                    <TextField label="Quote" value={props.quote} variant="outlined" />
+                    <TextField sx={textFieldStyles} label="Campaign" value={props.campaign} variant="outlined" />
+                    <TextField sx={textFieldStyles} label="Name" value={props.name} variant="outlined" />
+                    <TextField sx={textFieldStyles} label="Quote" value={props.quote} variant="outlined" />
                 </div>
                 <div className="form-buttonHolder">
                     <Button type='submit' variant="contained">Save</Button>
