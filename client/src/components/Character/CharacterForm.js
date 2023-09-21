3cdbef8bbddf5dfc938b83fd3bdc9cc1adfbc1ca
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-21 15:34:21 -0600

    Character card form buttons are now spaced evenly

--- client/src/components/Character/CharacterForm.js
+++ client/src/components/Character/CharacterForm.js
@@ -9,7 +9,7 @@ export default function CharacterForm(props) {
                 <TextField label="Campaign" value={props.campaign} variant="outlined" />
                 <TextField label="Name" value={props.name} variant="outlined" />
                 <TextField label="Quote" value={props.quote} variant="outlined" />
-                <div>
+                <div className="form-buttonHolder">
                     <Button type='submit' variant="contained">Save</Button>
                     <Button onClick={() => { props.setEditingCard(false) }} variant="contained">Cancel</Button>
                 </div>
