5ec1448ccf342c05ef92b74552e418ff755bcefb
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-22 07:40:22 -0600

    The character's props shouldn't change until it is saved, different states are to be for that purpose. Card text fields values are now editable.

--- client/src/components/Character/CharacterForm.js
+++ client/src/components/Character/CharacterForm.js
@@ -2,15 +2,28 @@ import { Box, TextField, Button } from "@mui/material";
 import './Character.css'
 
 export default function CharacterForm(props) {
-    const textFieldStyles = {marginBottom: '12px'};
+
+    function setCharacterName(event) {
+        props.setCharName(event.target.value);
+    }
+
+    function setCharacterCampaign(event) {
+        props.setCharCampaign(event.target.value);
+    }
+
+    function setCharacterQuote(event) {
+        props.setCharQuote(event.target.value);
+    }
+
+    const textFieldStyles = { marginBottom: '12px' };
     return (
         <form>
             <img className="img" src={props.image} alt="Character Image" />
             <Box padding={1}>
                 <div className="form-inputsHolder">
-                    <TextField sx={textFieldStyles} label="Campaign" value={props.campaign} variant="outlined" />
-                    <TextField sx={textFieldStyles} label="Name" value={props.name} variant="outlined" />
-                    <TextField sx={textFieldStyles} label="Quote" value={props.quote} variant="outlined" />
+                    <TextField sx={textFieldStyles} onChange={(event) => { setCharacterCampaign(event) }} label="Campaign" value={props.campaign} variant="outlined" />
+                    <TextField sx={textFieldStyles} onChange={(event) => { setCharacterName(event) }} label="Name" value={props.name} variant="outlined" />
+                    <TextField sx={textFieldStyles} onChange={(event) => { setCharacterQuote(event) }} label="Quote" value={props.quote} variant="outlined" />
                 </div>
                 <div className="form-buttonHolder">
                     <Button type='submit' variant="contained">Save</Button>
