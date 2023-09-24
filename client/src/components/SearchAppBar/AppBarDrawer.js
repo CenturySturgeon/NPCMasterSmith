3f1d985faa835ed04549558990f82c7696b1bb7c
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-23 19:22:55 -0600

    Drawer has icons and buttons

--- client/src/components/SearchAppBar/AppBarDrawer.js
+++ client/src/components/SearchAppBar/AppBarDrawer.js
@@ -1,17 +1,55 @@
-import { Drawer, List, ListItem, ListItemText } from '@mui/material';
+import {
+    Drawer,
+    Divider,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemIcon,
+    IconButton
+} from '@mui/material';
+import GroupsIcon from '@mui/icons-material/Groups';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
-export default function AppBarDrawer(props){
+export default function AppBarDrawer(props) {
     return (
-        <Drawer anchor="left" open={props.isDrawerOpen} onClose={props.toggleDrawer}>
-          <List>
-            <ListItem>
-              <ListItemText primary="Item 1" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Item 2" />
-            </ListItem>
-            {/* Add more menu items as needed */}
-          </List>
+        <Drawer anchor="left"
+            open={
+                props.isDrawerOpen
+            }
+            onClose={
+                props.toggleDrawer
+        }>
+            <div style={
+                {
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end'
+                }
+            }>
+                <IconButton onClick={
+                    props.toggleDrawer
+                }>
+                    <ChevronLeftIcon/>
+                </IconButton>
+            </div>
+
+            <Divider/>
+
+            <List>
+                <ListItem>
+                    <ListItemIcon>
+                        <PersonAddIcon/>
+                    </ListItemIcon>
+                    <ListItemText primary="Create Character"/>
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon>
+                        <GroupsIcon/>
+                    </ListItemIcon>
+                    <ListItemText primary="View Characters"/>
+                </ListItem>
+            </List>
         </Drawer>
     )
 }
