e8e560eac9de30a0fb703800b4692772b09aaac7
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-27 09:24:04 -0600

    Updated the character model id prop to match the other properties structure

--- server/models/models.go
+++ server/models/models.go
@@ -18,7 +18,7 @@ type Prompt struct {
 }
 
 type Character struct {
-	ID         int      `db:"id" json:"id" xml:"id" form:"id"`
+	ID         int      `db:"id" json:"Id" xml:"Id" form:"Id"`
 	Name       string   `db:"name" json:"Name" xml:"Name" form:"Name"`
 	Appearance string   `db:"appearance" json:"Appearance" xml:"Appearance" form:"Appearance"`
 	Quote      string   `db:"quote" json:"Quote" xml:"Quote" form:"Quote"`
