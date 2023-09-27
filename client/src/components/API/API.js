38b40f10088b6f085ce3512f7427b3579ad79000
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-27 16:57:37 -0600

    Added a delete function for the JS API

--- client/src/components/API/API.js
+++ client/src/components/API/API.js
@@ -1,5 +1,5 @@
 export class Character {
-    constructor(id, campaign, image, name, quote, appearance, roleplayProps){
+    constructor(id, campaign, image, name, quote, appearance, roleplayProps) {
         this.Id = parseInt(id);
         this.Campaign = campaign;
         this.Image = image;
@@ -10,29 +10,7 @@ export class Character {
     }
 }
 
-export function putCharacter(character){
-    const jsonData = JSON.stringify(character);
-
-    const url = '/putCharacter';
-
-    const requestOptions = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: jsonData,
-    };
-    
-    // Fetch should be returned so the call to the function can make use of the 'then' and 'catch' sentences to execute further logic
-    return fetch(url, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.status;
-        })
-}
-
+// POST request to create a new character
 export function postCharacter(data) {
 
     const jsonData = JSON.stringify(data);
@@ -46,7 +24,55 @@ export function postCharacter(data) {
         },
         body: jsonData,
     };
-    
+
+    // Fetch should be returned so the call to the function can make use of the 'then' and 'catch' sentences to execute further logic
+    return fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.status;
+        })
+}
+
+// PUT request to update the character
+export function putCharacter(character) {
+    const jsonData = JSON.stringify(character);
+
+    const url = '/putCharacter';
+
+    const requestOptions = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: jsonData,
+    };
+
+    // Fetch should be returned so the call to the function can make use of the 'then' and 'catch' sentences to execute further logic
+    return fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.status;
+        })
+}
+
+// DELETE request to delete the character
+export function deleteCharacter(character) {
+    const jsonData = JSON.stringify(character);
+
+    const url = '/deleteCharacter';
+
+    const requestOptions = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: jsonData,
+    };
+
     // Fetch should be returned so the call to the function can make use of the 'then' and 'catch' sentences to execute further logic
     return fetch(url, requestOptions)
         .then(response => {
