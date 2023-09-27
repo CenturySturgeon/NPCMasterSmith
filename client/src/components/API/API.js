2fe24d700b71fb685f6d075124e09766bf290c68
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-09-27 09:06:28 -0600

    Added PUT character function to the JS API

--- client/src/components/API/API.js
+++ client/src/components/API/API.js
@@ -10,6 +10,29 @@ export class Character {
     }
 }
 
+export function putCharacter(character){
+    const jsonData = JSON.stringify(character);
+
+    const url = '/putCharacter';
+
+    const requestOptions = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: jsonData,
+    };
+    
+    // Fetch should be returned so the call to the function can make use of the 'then' and 'catch' sentences to execute further logic
+    return fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.status;
+        })
+}
+
 export function testPOST(data) {
 
     const jsonData = JSON.stringify(data);
