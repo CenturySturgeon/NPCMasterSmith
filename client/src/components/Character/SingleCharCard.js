2dd091a4128b50fb6778dd2d8e702fc22542dfc5
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-10-06 09:21:54 -0600

    Added single character page

---
+++ client/src/components/Character/SingleCharCard.js
@@ -0,0 +1,20 @@
+import CharacterCard from "./CharacterCard"
+import man_image from '../../../public/images/profile_man.png'
+
+const SingleCharCard = () => {
+    return (
+        <CharacterCard
+            isOnlyCard={false}
+            id={0}
+            campaign={""}
+            image={man_image}
+            favorite={false}
+            name={"AI Created Character"}
+            quote={"Hello There"}
+            appearance={"My appearance"}
+            roleplayProps={["Prop1", "Prop2", "Prop3"]}
+        />
+    )
+}
+
+export default SingleCharCard;
