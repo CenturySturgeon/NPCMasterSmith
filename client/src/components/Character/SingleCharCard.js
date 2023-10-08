6acdba5344fac7126a5d87eb4237f1df204a1f9b
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-10-08 12:26:08 -0600

    Added single card title

--- client/src/components/Character/SingleCharCard.js
+++ client/src/components/Character/SingleCharCard.js
@@ -1,19 +1,25 @@
+import { Box, Typography, Button } from "@mui/material";
 import CharacterCard from "./CharacterCard"
 import man_image from '../../../public/images/profile_man.png'
 
 const SingleCharCard = () => {
     return (
-        <CharacterCard
-            isOnlyCard={true}
-            id={0}
-            campaign={""}
-            image={man_image}
-            favorite={false}
-            name={"AI Created Character"}
-            quote={"Hello There"}
-            appearance={"My appearance"}
-            roleplayProps={["Prop1", "Prop2", "Prop3"]}
-        />
+        <>
+
+            <Typography fontWeight={400} variant="h1" component="h1">Edit your Character</Typography>
+            <Box sx={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
+                <CharacterCard
+                    isOnlyCard={true}
+                    id={0}
+                    campaign={""}
+                    image={man_image}
+                    favorite={false}
+                    name={"AI Created Character"}
+                    quote={"Hello There"}
+                    appearance={"My appearance"}
+                    roleplayProps={["Prop1", "Prop2", "Prop3"]} />
+            </Box>
+        </>
     )
 }
 
