67456026349a5744b2ba6c6114ca5a846d568370
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-10-09 06:26:37 -0600

    Contentholder is now an Outlet to render children

--- client/src/components/ContentHolder/ContentHolder.js
+++ client/src/components/ContentHolder/ContentHolder.js
@@ -1,12 +1,11 @@
-import CharactersPage from "../Character/CharactersPage";
 import Box from '@mui/material/Box';
-import PromptPage from "../Prompt/PromptPage";
+import { Outlet } from "react-router-dom";
+
 
 const ContentHolder = () => {
     return (
         <Box sx={{ mb: '12px', mt: '12px', width: '100%', display: 'flex', flexGrow: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <PromptPage></PromptPage>
-            <CharactersPage/>
+            <Outlet />
         </Box>
     )
 }
