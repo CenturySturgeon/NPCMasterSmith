e0d476c76d64f8b984c52bfb543ec0a5501bbdf0
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-10-09 07:27:56 -0600

    PromptPage now redirects to /newcharacter with LLM data

--- client/src/components/Prompt/PromptPage.js
+++ client/src/components/Prompt/PromptPage.js
@@ -1,28 +1,32 @@
 import { useContext } from 'react';
 import { AppContext } from '../../App';
+import { Link, useNavigate } from 'react-router-dom';
 import { Prompt, postCharacterPrompt } from '../API/API';
-import PromptField from "./PromptField";
 import { Box, Typography, Button } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
-
-// Handles the LLM response
-function handlePostResponse(data) {
-    console.log(data, data.character);
-}
-
-const handlePromptExampleClick = (event) => {
-    prompt = new Prompt(event.target.textContent);
-
-    postCharacterPrompt(prompt)
-        // Handle the response if the request succeeds
-        .then(data => { handlePostResponse(data) })
-        .catch(error => {
-            console.error('Error:', error);
-            // Handle errors if the request fails
-        });
-}
+import PromptField from "./PromptField";
 
 const PromptPage = () => {
+    const navigate = useNavigate();
+
+    // Handles the LLM response
+    function handlePostResponse(data) {
+        console.log(data.character)
+        navigate('/newcharacter', { state: data.character });
+    }
+
+    const handlePromptExampleClick = (event) => {
+        prompt = new Prompt(event.target.textContent);
+
+        postCharacterPrompt(prompt)
+            // Handle the response if the request succeeds
+            .then(data => { handlePostResponse(data) })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle errors if the request fails
+            });
+    }
+
     // Extract the theme colors object from the app's context
     const { themeColors } = useContext(AppContext);
 
@@ -35,13 +39,13 @@ const PromptPage = () => {
                 <Typography fontWeight={400} variant="h1" component="h1">All Your NPCs In One Place</Typography>
             </Box>
             <Box width={'60%'} >
-                <Button color={buttonColor} sx={buttonStyles} variant="outlined" endIcon={ <SendIcon />} onClick={handlePromptExampleClick}>
+                <Button color={buttonColor} sx={buttonStyles} variant="outlined" endIcon={<SendIcon />} onClick={handlePromptExampleClick}>
                     A small, goblin-like hunchback creature who hides his dark intentions behind his charm.
                 </Button>
-                <Button color={buttonColor} sx={buttonStyles} variant="outlined" endIcon={ <SendIcon />} onClick={handlePromptExampleClick}>
+                <Button color={buttonColor} sx={buttonStyles} variant="outlined" endIcon={<SendIcon />} onClick={handlePromptExampleClick}>
                     A seasoned bartender who knows and talks more than he probably should...
                 </Button>
-                <Button color={buttonColor} sx={buttonStyles} variant="outlined" endIcon={ <SendIcon /> } onClick={handlePromptExampleClick}>
+                <Button color={buttonColor} sx={buttonStyles} variant="outlined" endIcon={<SendIcon />} onClick={handlePromptExampleClick}>
                     A shipyard store owner with a thick pirate accent, a rude demeanor, and a wooden leg.
                 </Button>
             </Box>
